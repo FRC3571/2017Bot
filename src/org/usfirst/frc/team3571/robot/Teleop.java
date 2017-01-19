2936e30aa9bf49c3d413774688852d735742015e
Furqan <Furqan@199.235.131.118>
2017-01-19 12:03:55 -0500

    teleop change

--- src/org/usfirst/frc/team3571/robot/Teleop.java
+++ src/org/usfirst/frc/team3571/robot/Teleop.java
@@ -3,7 +3,19 @@ package org.usfirst.frc.team3571.robot;
 import org.usfirst.frc.team3571.robot.utilities.XboxController.Button;
 
 public class Teleop extends OI {
-	
+
+	// Only a reference is being passed
+	// therefore the button keeps on functioning
+	static Button driveStopButton = driver.Buttons.B;
+
+	// Holds value for if triggers should be used instead of LeftStick Y
+	static boolean triggerDrive = false;
+	static Button triggerSwitchButton = driver.Buttons.LeftStick;
+
+	// Holds the current drive value
+	// which is invalid if the driver is holding button B
+	static double driveY = 0;
+
 	/**
 	 * The initialization code for Teleop
 	 */
@@ -16,5 +28,26 @@ public class Teleop extends OI {
 	 */
 	public static void periodic() {
 		// TODO Make Teleop Code
+
+		// Flips the state of triggerDrive when Button B changes state to
+		// pressed
+		if (triggerSwitchButton.changedDown) {
+			triggerDrive = !triggerDrive;
+		}
+
+		// Controls which Axis controls the robot base drive
+		if (triggerDrive) {
+			driveY = driver.Triggers.Combined;
+		} else {
+			driveY = driver.LeftStick.Y;
+		}
+		// The Above is the same as
+		// driveY = triggerDrive ? driver.Triggers.Combined : driver.LeftStick.Y;
+
+		if (!driveStopButton.current) {
+			drive.arcadeDrive(driveY, driver.LeftStick.X);
+		} else {
+			drive.stopMotor();
+		}
 	}
 }
