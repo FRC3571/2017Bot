5473403e0013e8c071310c6ad947dcf32042405a
Furqan <Furqan@199.235.128.160>
2017-05-25 15:21:24 -0400

    working code

--- src/org/usfirst/frc/team3571/robot/command/MyAuto.java
+++ src/org/usfirst/frc/team3571/robot/command/MyAuto.java
@@ -1,44 +1,26 @@
 package org.usfirst.frc.team3571.robot.command;
 
-//
 import org.usfirst.frc.team3571.robot.OI;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
+public class MyAuto extends Command {
 
-public class DefaultAuto extends Command {
-	long i = 0;
-	boolean limit = true;
-	Timer t1 = new Timer();
-	double timeElapsed = 0;
-	
-    public DefaultAuto() {
+    public MyAuto() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
+
     // Called just before this Command runs the first time
     protected void initialize() {
-    	OI.gyro.reset();
-    	t1.reset();
-    	t1.start();
     }
-    //public static final CameraModule camera = new CameraModule();
+
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
-    	timeElapsed = t1.get();
-    	if (timeElapsed < 2){
-    		OI.drive.arcadeDrive(0.3, 0);
-    	}
-    	else{
-    		OI.drive.arcadeDrive(0,0);
-    	}
-    	
+    	OI.drive.arcadeDrive(0.3, 0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
