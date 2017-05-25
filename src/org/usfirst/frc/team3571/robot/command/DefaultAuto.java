db5e1c778a5e9c2179894b08dafe10b8cefa73bc
Riya Anadkat <1anadkatriy@hdsb.ca>
2017-05-25 15:49:32 -0400

    automonous turning

--- src/org/usfirst/frc/team3571/robot/command/DefaultAuto.java
+++ src/org/usfirst/frc/team3571/robot/command/DefaultAuto.java
@@ -31,9 +31,16 @@ public class DefaultAuto extends Command {
      if (timeElapsed < 3){
     	 OI.drive.arcadeDrive(0.75,0);
      }
+     else if(timeElapsed < 5){
+    	 OI.drive.arcadeDrive(0.25,0.75);
+     }
+    	 else if(timeElapsed < 9){
+    		 OI.drive.arcadeDrive(0.50,-0.25);
+    }
      else {
     	 OI.drive.arcadeDrive(0,0);
      }
+    
     }
 
     // Make this return true when this Command no longer needs to run execute()
