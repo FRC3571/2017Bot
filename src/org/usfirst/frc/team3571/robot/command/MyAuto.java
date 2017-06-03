0c51e9b29060e183cf1af148de2e3958319e06fe
Krsna <Krsna@192.168.2.27>
2017-06-03 17:01:37 -0400

    for review

--- src/org/usfirst/frc/team3571/robot/command/MyAuto.java
+++ src/org/usfirst/frc/team3571/robot/command/MyAuto.java
@@ -1,11 +1,18 @@
 package org.usfirst.frc.team3571.robot.command;
 
+import edu.wpi.first.wpilibj.Timer;
+
+import org.usfirst.frc.team3571.robot.OI;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class MyAuto extends Command {
+	
+	Timer kd=new Timer();
+	double timeElapsed;
 
     public MyAuto() {
         // Use requires() here to declare subsystem dependencies
@@ -14,10 +21,20 @@ public class MyAuto extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	kd.reset();
+    	kd.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	timeElapsed=kd.get();
+    	
+    	if (timeElapsed<3)
+    		OI.drive.arcadeDrive(0.75,0);	//(speed,turning speed)
+    	else if (timeElapsed<6)
+    		OI.drive.arcadeDrive(0.5,0.5);
+    	else
+    		OI.drive.arcadeDrive(0,0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
