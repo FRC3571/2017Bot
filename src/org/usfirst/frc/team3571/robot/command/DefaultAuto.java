9c0ab8ee194fab3f7ddabbbd7196a2e10aff0c16
Riya Anadkat <1anadkatriy@hdsb.ca>
2017-06-08 15:46:11 -0400

    test .75 with 4 secs

--- src/org/usfirst/frc/team3571/robot/command/DefaultAuto.java
+++ src/org/usfirst/frc/team3571/robot/command/DefaultAuto.java
@@ -28,7 +28,7 @@ public class DefaultAuto extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {  
      timeElapsed = jaz.get();
-     if (timeElapsed < 4){
+     if (timeElapsed < 5){
     	 OI.drive.arcadeDrive(0.75,0);
     	 }
 
