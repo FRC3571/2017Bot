c912794a60abb19f34869fa1ec6c017fb6b5fa92
Jasiya Janjua <jasiya.janjua8@gmail.com>
2017-06-08 15:46:46 -0400

    5 second forward 50% speed

--- src/org/usfirst/frc/team3571/robot/command/MyAuto.java
+++ src/org/usfirst/frc/team3571/robot/command/MyAuto.java
@@ -29,7 +29,7 @@ public class MyAuto extends Command {
     protected void execute() {
     	timeElapsed = jaz.get();
     
-    	if (timeElapsed < 4){
+    	if (timeElapsed < 5){
     		OI.drive.arcadeDrive(0.50,0);
     	}
     	else{
