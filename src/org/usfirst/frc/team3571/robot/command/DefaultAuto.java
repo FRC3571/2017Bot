28ba52eea770b0ca3c0bae9a5ba6b5997c16023f
Tushar <1atmakurutus@hdsb.ca>
2017-06-08 15:45:56 -0400

    4 Seconds 100% Speed

--- src/org/usfirst/frc/team3571/robot/command/DefaultAuto.java
+++ src/org/usfirst/frc/team3571/robot/command/DefaultAuto.java
@@ -29,7 +29,7 @@ public class DefaultAuto extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {  
     	TimeElapsed = Tushar.get();
-    	if (TimeElapsed < 3){
+    	if (TimeElapsed < 4){
     		OI.drive.arcadeDrive(1,0);
     	}
     
